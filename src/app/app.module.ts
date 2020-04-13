55f7055d802ec8fd70f5637ed8a462d77551b0ae
chsal <chsalchsal@gmail.com>
2020-04-13 17:47:46 +1200

    Add header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { todos } from "./components/todos/todos.component";
 import { todo } from "./components/todo/todo.component";
+import { header } from "./components/layout/header/header.component";
 
 @NgModule({
-  declarations: [AppComponent, todos, todo],
+  declarations: [AppComponent, todos, todo, header],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/layout/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "Todo-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
+})
+export class header {}
