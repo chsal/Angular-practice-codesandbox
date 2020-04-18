7262f7f7dbee1ce9cecf2b4cafb7777849e0c1c4
chsal <chsalchsal@gmail.com>
2020-04-19 11:55:15 +1200

    Added add-todo component does nothing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { todos } from "./components/todos/todos.component";
 import { todo } from "./components/todo/todo.component";
 import { header } from "./components/layout/header/header.component";
+import { addTodo } from "./components/add-todo/add-todo.component";
 
 @NgModule({
-  declarations: [AppComponent, todos, todo, header],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, todos, todo, header, addTodo],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/components/add-todo/add-todo.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "add-todo",
+  templateUrl: "./add-todo.component.html",
+  styleUrls: ["./add-todo.component.css"]
+})
+export class addTodo implements Oninit {
+  title: string;
+
+  constructor() {}
+
+  ngOninti() {}
+}
