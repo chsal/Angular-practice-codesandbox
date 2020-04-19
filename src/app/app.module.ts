45441aa5646d8ff0871756f87ee25cc5c7a2f408
chsal <chsalchsal@gmail.com>
2020-04-19 15:24:48 +1200

    Add About page and add links to home and about in header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,25 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { todos } from "./components/todos/todos.component";
 import { todo } from "./components/todo/todo.component";
 import { header } from "./components/layout/header/header.component";
 import { addTodo } from "./components/add-todo/add-todo.component";
-import { RouterModule } from "@angular/router";
+import { about } from "./components/pages/about.component";
 
 @NgModule({
-  declarations: [AppComponent, todos, todo, header, addTodo],
+  declarations: [AppComponent, todos, todo, header, addTodo, about],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([{ path: "", component: todos }])
+    RouterModule.forRoot([
+      { path: "", component: todos },
+      { path: "about", component: about }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
